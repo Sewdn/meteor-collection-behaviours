8131ccef3b73e7a658688392097825956897b081
Jaak Hermans <jaak.hermans@redandivory.be>
2014-08-22 10:45:50 +0200

    use new date() on timestampable

--- behaviours/timestampable.js
+++ behaviours/timestampable.js
@@ -1,11 +1,11 @@
 CollectionBehaviours.defineBehaviour('timestampable', function(getTransform, args){
   var self = this;
   self.before.insert(function (userId, doc) {
-    doc.createdAt = Date.now();
+    doc.createdAt = new Date();
   });
   self.before.update(function (userId, doc, fieldNames, modifier, options) {
     if(!modifier.$set)
       modifier.$set = {};
-    modifier.$set.updatedAt = Date.now();
+    modifier.$set.updatedAt = new Date();
   });
 });
