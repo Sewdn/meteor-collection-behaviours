a4782ddc7406a98140d6233157689426a5955230
Pieter Soudan <pieter.soudan@redandivory.be>
2014-11-14 10:00:08 +0100

    backwards compatibility

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "Extends Meteor.Collection with behaviour patterns using CollectionHooks",
+  summary: "Extends Mongo.Collection with behaviour patterns using matb33:collection-hooks",
   "version": "0.2.0",
   "git": "https://github.com/Sewdn/meteor-collection-behaviours.git",
   "name": "meteor-collection-behaviours"
@@ -7,17 +7,28 @@ Package.describe({
 
 var both = ["client", "server"];
 
-Package.on_use(function (api, where) {
-  if(api.versionsFrom) {
-    api.versionsFrom('METEOR@0.9.0');
-  }
-  api.use([
-    "underscore",
-    "mongo-livedata",
-    "matb33:collection-hooks@0.7.6"
-  ], both);
+Package.onUse = Package.onUse || Package.on_use;    // backwards-compat
+Package.onTest = Package.onTest || Package.on_test; // backwards-compat
 
-  api.add_files([
+Package.onUse(function (api, where) {
+  api.addFiles = api.addFiles || api.add_files;     // backwards-compat
+
+  if(api.versionsFrom) {
+    api.versionsFrom('METEOR@0.9.1');
+    api.use([
+      'mongo',
+      'underscore',
+      'matb33:collection-hooks@0.7.6'
+    ], both);
+  } else {
+    api.use([
+      'mongo-livedata',
+      'underscore',
+      'collection-hooks'
+    ], both);
+  }
+
+  api.addFiles([
       "collection-behaviours.js",
       "behaviours/timestampable.js",
       "behaviours/softRemovable.js",
@@ -26,5 +37,6 @@ Package.on_use(function (api, where) {
       "behaviours/sortable.js",
       "behaviours/trackable.js"
   ], both);
+
   api.export("CollectionBehaviours");
 });
